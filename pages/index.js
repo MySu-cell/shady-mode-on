286aa0fab74611aac8d032cdf002dfd219347ff3
MySu-cell <myriamsudry777@gmail.com>
2025-05-28 21:34:18 +0200

    add pages/index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Welcome to ShadyOn 🎤</h1>
+      <p>This is the beginning of your Next.js project with Eminem power.</p>
+    </div>
+  );
+}
